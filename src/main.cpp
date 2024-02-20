31a2aa8652bdaab3aedf2ecd07ac40135feea1b7
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-02-20 10:45:53 +0100

    flabby bat

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,53 @@
 #include "config.h"
 #include "yume.h"
+#include "project.h"
 
 int main() {
 #pragma region INITIALIZATION
     /* ------------------------------------------------
     * ---------------- INITIALIZATION -----------------
-    * ------------------------------------------------ */
+    * ------------------------------------------------- */
     yumegl::init("Yume");
-    yumegl::eFunc::setColor(colour{0.72f, 0.72f, 0.72f, 1.0f});
+    yumegl::eFunc::setColor(colour::BLACK());
+
+    // CAMERA
+    object::Camera3D cam({ 0.0f, 4.0f, 3.0f }, yumegl::WINDOW_WIDTH, yumegl::WINDOW_HEIGHT, false);
 
     // DEPTH
     glEnable(GL_DEPTH_TEST);
 
     // OBJECTS
-    auto* bat = new rd1::Texture("../assets/textures/sonic_disco.png", mathy::vec3yu<>{ 0.6f, 0.0f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.045f, 0.08f });
-    bat->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
+    shaderSystem::Shader default3DShader;
+    default3DShader.genShader("../assets/shaders/3D/vertex.glsl", "../assets/shaders/3D/fragment.glsl");
 
-    auto* bug = new rd1::Texture("../assets/textures/sonic_warning.png", mathy::vec3yu<>{ -0.6f, 0.2f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.022f, 0.04f });
-    bug->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
+    auto* cube = new rd1::Cube("../assets/textures/sonic_disco.png", glm::vec3{ 0.0f, 4.0f, -3.0f }, glm::vec3{ 1.0f });
+    cube->shader.genShader("../assets/shaders/3D/vertex.glsl", "../assets/shaders/3D/fragment.glsl");
+    cube->setWindowSize(yumegl::WINDOW_WIDTH, yumegl::WINDOW_HEIGHT);
 
-    float batTimer{ 0.0f };
-    float batMax{ 1.0f };
+    auto* room = new rd1::Cube("../assets/textures/sonic_forest.png", glm::vec3{ 0.0f, 5.0f, 0.0f }, glm::vec3{ 20.0f, 10.0f, 20.0f });
+    room->setWindowSize(yumegl::WINDOW_WIDTH, yumegl::WINDOW_HEIGHT);
+
+    auto* floor = new rd1::Cube("../assets/textures/sonic_floor.png", glm::vec3{ 0.0f, -5.0f, 0.0f }, glm::vec3{ 20.0f, 0.1f, 20.0f });
+    floor->setWindowSize(yumegl::WINDOW_WIDTH, yumegl::WINDOW_HEIGHT);
+
+    auto* panel = new rd1::Texture("../assets/textures/sonic_ice.png", mathy::vec3yu<>{ 0.0f, 0.0f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.8f, 0.8f });
+    panel->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
 #pragma endregion
 
 #pragma region MAIN_LOOP
     /* ------------------------------------------------
     * ------------------ MAIN LOOP --------------------
     * ------------------------------------------------ */
-    //int frameCounter = 0;
-    //auto startTime = std::chrono::high_resolution_clock::now();
+    int frameCounter = 0;
+    auto startTime = std::chrono::high_resolution_clock::now();
 
     while (yumegl::isWindowOpen()) {
-        //auto frameStartTime = std::chrono::high_resolution_clock::now();
+        auto frameStartTime = std::chrono::high_resolution_clock::now();
 
         // UPDATE
         yumegl::update();
         input::update();
+        cam.update(yumegl::deltaTime);
 
         // INPUT SYSTEM
         if (input::keyPressed(GLFW_KEY_ESCAPE))
@@ -47,39 +59,58 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // ROOM
-        bat->bindTexture();
-        bat->render_ownShader();
-        bat->setRotation(mathy::vec3yu{ 0.0f, 0.0f, 1.0f }, 180.0f);
+        default3DShader.use();
 
-        if (bat->position.y() < 1.0f - bat->size.y()) {
-            bat->position.container.y += 1.0f * yumegl::deltaTime;
-        }
+        cam.update(default3DShader);
+        room->bindTexture();
+        room->render_foregoingShader();
+        room->setRotation_getShader(glm::vec3{ 0.1f, 0.1f, 0.1f }, default3DShader, 0.1f);
 
-        if (!input::keyDown(GLFW_KEY_SPACE)) batTimer = 0.0f;
-        if (input::keyDown(GLFW_KEY_SPACE) && batTimer < batMax) {
-            bat->position.container.y -= 2.0f * yumegl::deltaTime;
-            batTimer += 1.0f * yumegl::deltaTime;
-        }
+        cam.update(default3DShader);
+        floor->bindTexture();
+        floor->render_foregoingShader();
+        floor->setRotation_getShader(glm::vec3{ 0.1f, 0.1f, 0.1f }, default3DShader, 0.1f);
 
-        bat->updateVertices();
-        bat->refresh();
+        // CUBE
+        cube->shader.use();
+        cam.update(cube->shader);
+        cube->bindTexture();
+        cube->render_ownShader();
+        cube->rotate(glm::vec3{ 0.6f, 0.8f, 0.6f }, 1.0f);
 
-        if (bat->position.distance(bug->position) < bat->size.y()) {
-            bug->enable = false;
-        }
-
-        if (bug->enable) {
-            bug->bindTexture();
-            bug->render_ownShader();
-            bug->setRotation(mathy::vec3yu{ 0.0f, 0.0f, 1.0f }, 180.0f);
-
-            bug->position.container.x += 0.5f * yumegl::deltaTime;
-            if (bug->position.x() > 1.0f) {
-                bug->position.container.x = -1.0f;
+        // GUI
+        if (!cam.active) {
+            // MATHY TRANSFORMATIONS | YUME MATH IMPLEMENTATION AND USE EXAMPLE
+            if (input::keyDown(GLFW_KEY_RIGHT)) {
+                panel->size.container.x = panel->size.x() + 0.0005f;
+                panel->updateVertices();
+                panel->refresh();
+            }
+            else if (input::keyDown(GLFW_KEY_LEFT)) {
+                panel->size.container.x = panel->size.x() - 0.0005f;
+                panel->updateVertices();
+                panel->refresh();
+            }
+            if (input::keyDown(GLFW_KEY_UP)) {
+                panel->size.container.y = panel->size.y() + 0.0005f;
+                panel->updateVertices();
+                panel->refresh();
+            }
+            else if (input::keyDown(GLFW_KEY_DOWN)) {
+                panel->size.container.y = panel->size.y() - 0.0005f;
+                panel->updateVertices();
+                panel->refresh();
             }
 
-            bug->updateVertices();
-            bug->refresh();
+            if (input::keyDown(GLFW_KEY_R)) {
+                panel->rotate(mathy::vec3yu<>{0.0f, 0.0f, 1.0f}, 1.0f);
+                panel->updateVertices();
+                panel->refresh();
+            }
+
+            panel->bindTexture();
+            panel->render_ownShader();
+            panel->setRotation(mathy::vec3yu<>{ 0.0f, 0.0f, 1.0f }, 180.0f);
         }
 
         // SWAP POLL EVENTS
@@ -88,7 +119,6 @@ int main() {
         /* ------------------------------------------------
          * -------------------- STATS ---------------------
          * ------------------------------------------------ */
-        /*
         frameCounter++;
 
         if (frameCounter == 60) {
@@ -96,6 +126,7 @@ int main() {
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
             std::cout << "\nThe time it takes to generate 60 frames: >" << duration.count() << "< ms\n";
             std::cout << "The limit od milliseconds to generate 60 frames: >960/1000<\n";
+            std::cout << yumegl::deltaTime << ": deltaTime\n";
 
             frameCounter = 0;
             startTime = std::chrono::high_resolution_clock::now();
@@ -103,7 +134,6 @@ int main() {
 
         auto frameEndTime = std::chrono::high_resolution_clock::now();
         auto frameDuration = std::chrono::duration_cast<std::chrono::milliseconds>(frameEndTime - frameStartTime);
-        */
     }
 #pragma endregion
 
@@ -111,9 +141,12 @@ int main() {
     /* ------------------------------------------------
     * -------------- DE-INITIALIZATION ----------------
     * ------------------------------------------------ */
-    delete bat;
-    delete bug;
+    delete cube;
+    delete room;
+    delete floor;
+    delete panel;
 
+    shaderSystem::deleteShader(default3DShader);
     yumegl::eExit::close();
 
     return 0;
