e144ba7455e83901b2bf635dbaed6851d7591b5a
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-02-22 15:09:45 +0100

    moving ground

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,20 @@ int main() {
     * ---------------- INITIALIZATION -----------------
     * ------------------------------------------------- */
     yumegl::init("yumegl");
-    yumegl::eFunc::setColor(colour::YELLOW());
+    yumegl::eFunc::setColor(colour::BLACK());
 
     // DEPTH
     glEnable(GL_DEPTH_TEST);
 
     // OBJECTS
-    auto* panel = new rd1::Texture("../assets/textures/sonic_disco.png", mathy::vec3yu<>{ 0.0f, 0.0f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.18f, 0.32f });
+    auto* panel = new rd1::Texture("../assets/textures/sonic_warning.png", mathy::vec3yu<>{ 0.5f, 0.59f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.09f, 0.16f });
     panel->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
+
+    auto* ground = new rd1::Texture("../assets/textures/sonic_dirt.png", mathy::vec3yu<>{ 0.0f, 1.75f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 1.00f, 1.0f });
+    ground->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
+
+    auto* ground1 = new rd1::Texture("../assets/textures/sonic_dirt.png", mathy::vec3yu<>{ -2.0f, 1.75f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 1.00f, 1.0f });
+    ground1->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
 #pragma endregion
 
 #pragma region MAIN_LOOP
@@ -27,18 +33,28 @@ int main() {
         yumegl::update();
         input::update();
 
+        ground->position.container.x += 0.4f * yumegl::deltaTime;
+        ground->updateVertices();
+        ground->refresh();
+
+        if (ground->position.x() > 1.0f + ground->size.x()) {
+            ground->position.container.x = -1.0f - ground->size.x();
+        }
+
+        ground1->position.container.x += 0.4f * yumegl::deltaTime;
+        ground1->updateVertices();
+        ground1->refresh();
+
+        if (ground1->position.x() > 1.0f + ground1->size.x()) {
+            ground1->position.container.x = -1.0f - ground1->size.x();
+        }
+
         // INPUT SYSTEM
         if (input::keyPressed(GLFW_KEY_ESCAPE))
             yumegl::setWindowStatus(false);
 
-        if (input::keyDown(GLFW_KEY_UP)) 
-            panel->position.container.y -= 1.0f * yumegl::deltaTime;
-        else if (input::keyDown(GLFW_KEY_DOWN))
-            panel->position.container.y += 1.0f * yumegl::deltaTime;
-        if (input::keyDown(GLFW_KEY_RIGHT))
-            panel->position.container.x -= 1.0f * yumegl::deltaTime;
-        else if (input::keyDown(GLFW_KEY_LEFT))
-            panel->position.container.x += 1.0f * yumegl::deltaTime;
+        if (input::keyDown(GLFW_KEY_SPACE)) 
+            // JUMP
 
         panel->updateVertices();
         panel->refresh();
@@ -52,6 +68,14 @@ int main() {
         panel->bindTexture();
         panel->render_ownShader();
         panel->setRotation(mathy::vec3yu<>{ 0.0f, 0.0f, 1.0f }, 180.0f);
+
+        ground->bindTexture();
+        ground->render_ownShader();
+        ground->setRotation(mathy::vec3yu<>{ 0.0f, 0.0f, 1.0f }, 180.0f);
+
+        ground1->bindTexture();
+        ground1->render_ownShader();
+        ground1->setRotation(mathy::vec3yu<>{ 0.0f, 0.0f, 1.0f }, 180.0f);
 
         // SWAP POLL EVENTS
         yumegl::swapBuffersPollEvents();
