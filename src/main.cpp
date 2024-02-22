7e16d89bdcdd18cc78e76580a2d1ce23134375df
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-02-22 22:06:27 +0100

    jump

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,12 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     // OBJECTS
-    auto* panel = new rd1::Texture("../assets/textures/sonic_warning.png", mathy::vec3yu<>{ 0.5f, 0.59f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.09f, 0.16f });
-    panel->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
+    auto* player = new rd1::Texture("../assets/textures/sonic_warning.png", mathy::vec3yu<>{ 0.5f, 0.6f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 0.09f, 0.16f });
+    player->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
+    float jumpTime{ 0.6f };
+    float jumpTimer{ jumpTime };
+    float jumpForce{ 1.1f };
+    bool jumping{ false };
 
     auto* ground = new rd1::Texture("../assets/textures/sonic_dirt.png", mathy::vec3yu<>{ 0.0f, 1.75f, 0.0f }, colour{ 0.0f, 0.0f, 0.0f, 1.0f }, mathy::vec2yu<>{ 1.00f, 1.0f });
     ground->shader.genShader("../assets/shaders/texture/vertex.glsl", "../assets/shaders/texture/fragment.glsl");
@@ -33,6 +37,7 @@ int main() {
         yumegl::update();
         input::update();
 
+        // PLATFORM MOVEMENT
         ground->position.container.x += 0.4f * yumegl::deltaTime;
         ground->updateVertices();
         ground->refresh();
@@ -49,15 +54,33 @@ int main() {
             ground1->position.container.x = -1.0f - ground1->size.x();
         }
 
+        // PLAYER JUMPING
+        if (!jumping) {
+            if (player->position.y() <= 0.59f)
+                player->position.container.y += 0.8f * yumegl::deltaTime;
+            else
+                jumping = false;
+        }
+        else {
+            if (jumpTimer > 0.0f) {
+                player->position.container.y -= jumpForce * yumegl::deltaTime;
+                jumpTimer -= 1.0f * yumegl::deltaTime;
+            }
+            else {
+                jumping = false;
+                jumpTimer = jumpTime;
+            }
+        }
+
         // INPUT SYSTEM
         if (input::keyPressed(GLFW_KEY_ESCAPE))
             yumegl::setWindowStatus(false);
 
-        if (input::keyDown(GLFW_KEY_SPACE)) 
-            // JUMP
+        if (input::keyPressed(GLFW_KEY_SPACE))
+            jumping = true;
 
-        panel->updateVertices();
-        panel->refresh();
+        player->updateVertices();
+        player->refresh();
 
         /* ------------------------------------------------
          * ------------------- RENDER ---------------------
@@ -65,9 +88,9 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // TEXTURE
-        panel->bindTexture();
-        panel->render_ownShader();
-        panel->setRotation(mathy::vec3yu<>{ 0.0f, 0.0f, 1.0f }, 180.0f);
+        player->bindTexture();
+        player->render_ownShader();
+        player->setRotation(mathy::vec3yu<>{ 0.0f, 0.0f, 1.0f }, 180.0f);
 
         ground->bindTexture();
         ground->render_ownShader();
@@ -86,7 +109,9 @@ int main() {
     /* ------------------------------------------------
     * -------------- DE-INITIALIZATION ----------------
     * ------------------------------------------------- */
-    delete panel;
+    delete player;
+    delete ground;
+    delete ground1;
 
     yumegl::eExit::close();
     return 0;
